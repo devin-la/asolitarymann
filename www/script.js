a40f02109b906a2f65260b6ccab875ef9d1b8946
Devin Smith <github@arzynik.com>
2015-10-13 21:53:06 -0700

    progress

--- www/script.js
+++ www/script.js
@@ -1,9 +1,6 @@
-
-
-var app = angular.module('asolitarymann', ['ngRoute']);
+var app = angular.module('asolitarymann', ['ngRoute', 'duScroll']);
 
 app.config(function($locationProvider, $routeProvider) {
-//app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
 	$routeProvider.when('/', {
 		template: ''
@@ -23,21 +20,41 @@ app.config(function($locationProvider, $routeProvider) {
 
 	$locationProvider.html5Mode({
 		enabled: true,
-		requireBase: false
+		requireBase: true
 	});
 });
-//}]);
 
-
-app.controller('main', function($scope) {
+app.controller('main', function($scope, $timeout) {
+	$timeout(function() {
+		$scope.loaded = true;
+	});
 	
+
+	$('.trailer').click(function() {
+		$(this).addClass('play');
+		$('#vimeo').attr('src','https://player.vimeo.com/video/124823784?autoplay=1'); 
+	});
+
 });
 
 app.run(function($rootScope, $location, $anchorScroll) {
+
+	if (!window.history || !history.replaceState) {
+		return;
+	}
+	$rootScope.$on('duScrollspy:becameActive', function($event, $element, $target){
+		var hash = $element.prop('hash');
+		console.log(hash);
+		if (hash == '#top') {
+			hash = '';
+		}
+		history.replaceState(null, null, hash.replace('#',''));
+	});
+
 	$rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
 		setTimeout(function() {
 			var hash = $location.$$path.replace('/','');
-			var el = $('a[name="' + hash + '"]');
+			var el = $('#' + hash);
 			if (!el.length) {
 				return;
 			}
@@ -49,32 +66,9 @@ app.run(function($rootScope, $location, $anchorScroll) {
 	});
 });
 
-
 $(function() {
-	var time = 600;
-	time = 0;
-	$('.trailer').click(function() {
-		$(this).addClass('play');
-		$('#vimeo').attr('src','https://player.vimeo.com/video/124823784?autoplay=1'); 
-	});
-
 	$('.trailer').parallax({imageSrc: '/images/trailer.jpg', speed : .4, positionY : '0px'});
 	$('.gallery').parallax({imageSrc: '/images/gallery001.jpg', speed : .4, positionY : '0px'});
 	$('.about-jeremy-pic').parallax({imageSrc: '/images/jeremy.jpg', speed : .4, positionY : '0px'});
 	$('.ost').parallax({imageSrc: '/images/ostbg.jpg', speed : .4, positionY : '-1800px'});
-
-
-
-	setTimeout(function() {
-		$('body').addClass('loaded');
-	}, time);
-/*
-	$('a').click(function(){
-		$('html, body').animate({
-			scrollTop: $( $.attr(this, 'href') ).offset().top
-		}, 500);
-		return false;
-	});
-	*/
-
 });
